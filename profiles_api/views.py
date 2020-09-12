05c73806b91e0635743e1f09b28153850e820a48
prakhartandon <prakhar.tandon.1994@gmail.com>
2020-09-13 00:28:44 +0530

    Added Put, patch and Delete methods in ApI VIew

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -4,11 +4,14 @@
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from profiles_api import serializers
 
 
 class HelloApiView(APIView):
     #HelloApiView
     """TEST API VIEW"""
+    serializer_class= serializers.HelloSerializer
 
 
     def get(self , request, format=None):
@@ -22,3 +25,31 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message':'Hello!', 'an_apiiview': an_apiiview})
+
+    def post(self, request):
+        """Create a hello message with our name"""
+
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name= serializer.validated_data.get('name')
+            message= f'Hello{name}'
+            return Response({'message':message})
+        else:
+            return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def put(self, request, pk=None):
+        """Handle updating an object"""
+        return Response({'method': 'PUT'})
+
+
+    def patch(self, request, pk=None):
+        """Handle partial update of an Object"""
+        return Response({'method': 'PATCH'})
+
+    def delete(self, request, pk=None):
+        """delete an Object"""
+        return Response({'method': 'DELETE'})
